1f6d8b73d82e1cb63e40bbd9e435df8e7653bfea
Taoufiq El hattami <taoufiq.el.hattami@actency.fr>
2024-04-01 17:24:31 +0200

    Add ConfigLoader

---
+++ lib/ConfigLoader.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+import configparser
+
+def get_config(env:str):
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read('conf/sbdl.conf')
+    conf = {}
+    for (key, val) in config.items(env):
+        conf[key] = val
+    return conf
+
+def get_spark_conf(env:str):
+    spark_conf = SparkConf()
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read('conf/spark.conf')
+    for(key, val) in config.items(env):
+        spark_conf.set(key, val)
+    return spark_conf
--- lib/Utils.py
+++ lib/Utils.py
@@ -2,23 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 import configparser
 
-def get_config(env:str):
-    config = configparser.ConfigParser()
-    config.optionxform = str
-    config.read('conf/sbdl.conf')
-    conf = {}
-    for (key, val) in config.items(env):
-        conf[key] = val
-    return conf
+from lib.ConfigLoader import get_spark_conf
 
-def get_spark_conf(env:str):
-    spark_conf = SparkConf()
-    config = configparser.ConfigParser()
-    config.optionxform = str
-    config.read('conf/spark.conf')
-    for(key, val) in config.items(env):
-        spark_conf.set(key, val)
-    return spark_conf
 
 def get_spark_session(env:str) -> SparkSession:
     conf = get_spark_conf(env)
