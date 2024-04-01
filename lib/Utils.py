a2f9798508d375b8f1e79dc221fd09efd1df49c5
Taoufiq El hattami <taoufiq.el.hattami@actency.fr>
2024-04-01 17:19:49 +0200

    UTils : fix  get_config

--- lib/Utils.py
+++ lib/Utils.py
@@ -2,17 +2,20 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 import configparser
 
-def get_sbdl_conf(env:str):
+def get_config(env:str):
     config = configparser.ConfigParser()
     config.optionxform = str
     config.read('conf/sbdl.conf')
-    return config.items(env)
+    conf = {}
+    for (key, val) in config.items(env):
+        conf[key] = val
+    return conf
 
 def get_spark_conf(env:str):
+    spark_conf = SparkConf()
     config = configparser.ConfigParser()
     config.optionxform = str
     config.read('conf/spark.conf')
-    spark_conf = SparkConf()
     for(key, val) in config.items(env):
         spark_conf.set(key, val)
     return spark_conf
