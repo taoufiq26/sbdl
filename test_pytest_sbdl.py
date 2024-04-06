994d3aa710bd63c6ca86e00a292a98d5bd5f6304
Taoufiq El hattami <taoufiq.el.hattami@actency.fr>
2024-04-06 02:52:38 +0200

    Another tests...

--- test_pytest_sbdl.py
+++ test_pytest_sbdl.py
@@ -17,44 +17,44 @@ def spark():
 
 @pytest.fixture(scope='session')
 def expected_party_rows():
-    return [Row(load_date=date(2022, 8, 2), account_id='6982391060',
-                party_id='9823462810', relation_type='F-N', relation_start_date=datetime(2019, 7, 29, 6, 21, 32)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391061', party_id='9823462811', relation_type='F-N',
+    return [Row(load_date=date(2022, 8, 2), account_id=6982391060,
+                party_id=9823462810, relation_type='F-N', relation_start_date=datetime(2019, 7, 29, 6, 21, 32)),
+            Row(load_date=date(2022, 8, 2), account_id=6982391061, party_id=9823462811, relation_type='F-N',
                 relation_start_date=datetime(2018, 8, 31, 5, 27, 22)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391062', party_id='9823462812', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391062, party_id=9823462812, relation_type='F-N',
                 relation_start_date=datetime(2018, 8, 25, 15, 50, 29)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391063', party_id='9823462813', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391063, party_id=9823462813, relation_type='F-N',
                 relation_start_date=datetime(2018, 5, 11, 7, 23, 28)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391064', party_id='9823462814', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391064, party_id=9823462814, relation_type='F-N',
                 relation_start_date=datetime(2019, 6, 6, 14, 18, 12)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391065', party_id='9823462815', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391065, party_id=9823462815, relation_type='F-N',
                 relation_start_date=datetime(2019, 5, 4, 5, 12, 37)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391066', party_id='9823462816', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391066, party_id=9823462816, relation_type='F-N',
                 relation_start_date=datetime(2019, 5, 15, 10, 39, 29)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462817', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391067, party_id=9823462817, relation_type='F-N',
                 relation_start_date=datetime(2018, 5, 16, 9, 53, 4)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391068', party_id='9823462818', relation_type='F-N',
+            Row(load_date=date(2022, 8, 2), account_id=6982391068, party_id=9823462818, relation_type='F-N',
                 relation_start_date=datetime(2017, 11, 27, 1, 20, 12)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462820', relation_type='F-S',
+            Row(load_date=date(2022, 8, 2), account_id=6982391067, party_id=9823462820, relation_type='F-S',
                 relation_start_date=datetime(2017, 11, 20, 14, 18, 5)),
-            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462821', relation_type='F-S',
+            Row(load_date=date(2022, 8, 2), account_id=6982391067, party_id=9823462821, relation_type='F-S',
                 relation_start_date=datetime(2018, 7, 19, 18, 56, 57))]
 
 
 @pytest.fixture(scope='session')
 def parties_list():
     return [
-        (date(2022, 8, 2), '6982391060', '9823462810', 'F-N', datetime.fromisoformat('2019-07-29 06:21:32.000+05:30')),
-        (date(2022, 8, 2), '6982391061', '9823462811', 'F-N', datetime.fromisoformat('2018-08-31 05:27:22.000+05:30')),
-        (date(2022, 8, 2), '6982391062', '9823462812', 'F-N', datetime.fromisoformat('2018-08-25 15:50:29.000+05:30')),
-        (date(2022, 8, 2), '6982391063', '9823462813', 'F-N', datetime.fromisoformat('2018-05-11 07:23:28.000+05:30')),
-        (date(2022, 8, 2), '6982391064', '9823462814', 'F-N', datetime.fromisoformat('2019-06-06 14:18:12.000+05:30')),
-        (date(2022, 8, 2), '6982391065', '9823462815', 'F-N', datetime.fromisoformat('2019-05-04 05:12:37.000+05:30')),
-        (date(2022, 8, 2), '6982391066', '9823462816', 'F-N', datetime.fromisoformat('2019-05-15 10:39:29.000+05:30')),
-        (date(2022, 8, 2), '6982391067', '9823462817', 'F-N', datetime.fromisoformat('2018-05-16 09:53:04.000+05:30')),
-        (date(2022, 8, 2), '6982391068', '9823462818', 'F-N', datetime.fromisoformat('2017-11-27 01:20:12.000+05:30')),
-        (date(2022, 8, 2), '6982391067', '9823462820', 'F-S', datetime.fromisoformat('2017-11-20 14:18:05.000+05:30')),
-        (date(2022, 8, 2), '6982391067', '9823462821', 'F-S', datetime.fromisoformat('2018-07-19 18:56:57.000+05:30'))]
+        (date(2022, 8, 2), 6982391060, 9823462810, 'F-N', datetime.fromisoformat('2019-07-29 06:21:32.000')),
+        (date(2022, 8, 2), 6982391061, 9823462811, 'F-N', datetime.fromisoformat('2018-08-31 05:27:22.000')),
+        (date(2022, 8, 2), 6982391062, 9823462812, 'F-N', datetime.fromisoformat('2018-08-25 15:50:29.000')),
+        (date(2022, 8, 2), 6982391063, 9823462813, 'F-N', datetime.fromisoformat('2018-05-11 07:23:28.000')),
+        (date(2022, 8, 2), 6982391064, 9823462814, 'F-N', datetime.fromisoformat('2019-06-06 14:18:12.000')),
+        (date(2022, 8, 2), 6982391065, 9823462815, 'F-N', datetime.fromisoformat('2019-05-04 05:12:37.000')),
+        (date(2022, 8, 2), 6982391066, 9823462816, 'F-N', datetime.fromisoformat('2019-05-15 10:39:29.000')),
+        (date(2022, 8, 2), 6982391067, 9823462817, 'F-N', datetime.fromisoformat('2018-05-16 09:53:04.000')),
+        (date(2022, 8, 2), 6982391068, 9823462818, 'F-N', datetime.fromisoformat('2017-11-27 01:20:12.000')),
+        (date(2022, 8, 2), 6982391067, 9823462820, 'F-S', datetime.fromisoformat('2017-11-20 14:18:05.000')),
+        (date(2022, 8, 2), 6982391067, 9823462821, 'F-S', datetime.fromisoformat('2018-07-19 18:56:57.000'))]
 
 
 @pytest.fixture(scope='session')
@@ -194,9 +194,9 @@ def test_read_parties_row(spark, expected_party_rows):
 
 
 def test_read_parties(spark, parties_list):
-    expected_df = spark.createDataFrame(parties_list)
+    expected_df = spark.createDataFrame(parties_list).toDF("load_date", "account_id", "party_id", "relation_type", "relation_start_date")
     actual_df = DataLoader.load_parties(spark, "LOCAL", False, None)
-    assert_df_equality(expected_df, actual_df, ignore_schema=True)
+    assert_df_equality(expected_df, actual_df)
 
 
 def test_read_party_schema(spark, parties_list):
@@ -209,7 +209,7 @@ def test_get_contract(spark, expected_contract_df):
     accounts_df = DataLoader.load_accounts(spark, "LOCAL", False, None)
     actual_contract_df = Transformations.get_contract(accounts_df)
     assert expected_contract_df.collect() == actual_contract_df.collect()
-    assert_df_equality(expected_contract_df, actual_contract_df, ignore_schema=True)
+    assert_df_equality(expected_contract_df, actual_contract_df)
 
 
 def test_kafka_kv_df(spark, expected_final_df):
@@ -223,4 +223,5 @@ def test_kafka_kv_df(spark, expected_final_df):
     data_df = Transformations.join_contract_party(contract_df, party_address_df)
     actual_final_df = Transformations.apply_header(spark, data_df) \
         .select("keys", "payload")
-    assert_df_equality(actual_final_df, expected_final_df, ignore_schema=True)
+    assert actual_final_df == expected_final_df
+    # assert_df_equality(actual_final_df, expected_final_df)
